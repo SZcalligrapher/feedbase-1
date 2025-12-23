416ae780115de5f5fdbfadcfbfd5844efda96783
Haoan Luo <111784618+SZcalligrapher@users.noreply.github.com>
2025-12-23 13:18:18 +0800

    <feat> 将 My Board 按钮和 NavTabs 放在同一个容器中

--- apps/web/components/layout/sidebar.tsx
+++ apps/web/components/layout/sidebar.tsx
@@ -29,26 +29,28 @@ export default async function Sidebar({
         {/* Projects */}
         <ProjectDropdown projects={projects} activeProject={currentProject} />
 
-        {/* Main Tabs */}
-        <NavTabs tabs={tabs} initialTabIndex={activeTabIndex} projectSlug={currentProject.slug} />
+        {/* Main Tabs with My Board */}
+        <div className='flex flex-col gap-2'>
+          <NavTabs tabs={tabs} initialTabIndex={activeTabIndex} projectSlug={currentProject.slug} />
+          
+          {/* My Board Button */}
+          <a href={feedbackUrl} target='_blank' rel='noopener noreferrer'>
+            <Button
+              variant='secondary'
+              className={cn(
+                'text-foreground/[85%] hover:text-foreground w-full items-center justify-start gap-1 border border-transparent p-1 font-light',
+                isFeedbackPage && 'bg-secondary text-foreground hover:bg-secondary'
+              )}>
+              {/* Icon */}
+              <div className='flex transform-none flex-row items-center justify-center p-1'>
+                <Clipboard className='h-5 w-5' />
+              </div>
 
-        {/* My Board Button */}
-        <a href={feedbackUrl} target='_blank' rel='noopener noreferrer'>
-          <Button
-            variant='secondary'
-            className={cn(
-              'text-foreground/[85%] hover:text-foreground w-full items-center justify-start gap-1 border border-transparent p-1 font-light',
-              isFeedbackPage && 'bg-secondary text-foreground hover:bg-secondary'
-            )}>
-            {/* Icon */}
-            <div className='flex transform-none flex-row items-center justify-center p-1'>
-              <Clipboard className='h-5 w-5' />
-            </div>
-
-            {/* Title */}
-            My Board
-          </Button>
-        </a>
+              {/* Title */}
+              My Board
+            </Button>
+          </a>
+        </div>
       </div>
     </div>
   );
