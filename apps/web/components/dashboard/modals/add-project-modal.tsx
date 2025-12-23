83221cc1bc33c452257e92720ca00a2bdee7397b
Haoan Luo <111784618+SZcalligrapher@users.noreply.github.com>
2025-12-23 12:53:56 +0800

    <feat> 更新slug位置

--- apps/web/components/dashboard/modals/add-project-modal.tsx
+++ apps/web/components/dashboard/modals/add-project-modal.tsx
@@ -100,19 +100,19 @@ export default function AddProjectDialog({ trigger }: { trigger: React.ReactNode
             </div>
 
             <div className='bg-background focus-within:ring-ring ring-offset-root flex h-9 w-full rounded-md border text-sm font-extralight transition-shadow duration-200 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-1'>
+              <div className='text-foreground/50 bg-accent flex select-none items-center rounded-l-md border-r px-3 py-2'>
+                {process.env.NEXT_PUBLIC_ROOT_DOMAIN}/
+              </div>
               <Input
                 className='h-full w-full border-0 bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0'
                 placeholder='feedbase'
                 value={slug}
                 onChange={handleSlugChange}
               />
-              <div className='text-foreground/50 bg-accent flex select-none items-center rounded-r-md border-l px-3 py-2'>
-                .{process.env.NEXT_PUBLIC_ROOT_DOMAIN}
-              </div>
             </div>
 
             <Label className='text-foreground/50 text-xs font-extralight'>
-              The subdomain of your project.
+              Your project will be available at {process.env.NEXT_PUBLIC_ROOT_DOMAIN}/{slug || 'slug'}
             </Label>
           </div>
         </div>
